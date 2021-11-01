ef4e9572c6d744462d05f81a9ee939d981eec35f
Николай Яшин <60584121+NikolayYashin@users.noreply.github.com>
2021-11-02 00:26:11 +0700

    Add files via upload

---
+++ t4.py
@@ -0,0 +1,26 @@
+import telebot
+
+bot = telebot.TeleBot('2060953992:AAEPGPw26rFcC7bASEuW2s2WX_Ku5dPQ6gg')
+
+chatID = -1001420169796
+
+print("Бот запущен. Нажмите Ctrl+C для завершения")
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, 'Привет! Для помощи наберите команду /help и будет помощь')
+
+@bot.message_handler(commands=["help"])
+def start(message):
+    bot.send_message(message.chat.id, 'Тут сообщение о помощи')
+
+
+@bot.message_handler(content_types=['text'])
+def all_messages(message):
+    if 'входящий платеж:' in message.text.lower():
+        bot.forward_message(chatID, message.chat.id, message.message_id)
+    if 'перевод от' in message.text.lower():
+        bot.forward_message(chatID, message.chat.id, message.message_id)
+
+if __name__ == '__main__':
+    bot.polling(none_stop = True)
